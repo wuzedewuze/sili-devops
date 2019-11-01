d8c0397e326db92c836935460fb62dc02ed6b10f
wu_yang <wuyang@xiniaoyun.com>
2019-11-01 15:48:33 +0800

    first

---
+++ sili_user/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SiliUserConfig(AppConfig):
+    name = 'sili_user'
---
+++ sili_user/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    name = models.CharField("姓名", max_length=32, null=True, help_text="姓名")
+    phone = models.CharField("电话", max_length=11, null=True, help_text="手机号")
+
+    class Meta:
+        verbose_name = "用户"
+        ordering = ["id"]
+
+    def __str__(self):
+        return self.username
